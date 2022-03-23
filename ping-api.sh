22e46e2be2d22abc465a51e608caa4ffdcff568c
DeadlyChambers <shanechambers85@gmail.com>
2022-03-23 03:41:29 -0600

    adding roles and bindings

--- ping-api.sh
+++ ping-api.sh
@@ -1,10 +1,13 @@
 echo -e "Running Api System Call"
 
 export system_port=$(kubectl get services/system-api -o go-template='{{(index .spec.ports 0).nodePort}}')
+echo -e "System Api: ${minikube ip):${system_port}"
 curl "http://$(minikube ip):${system_port}/System"
 
-echo -e "\nRunning Api Data Call"
+echo -e ""
+echo -e "Running Api Data Call"
 
 export data_port=$(kubectl get services/data-api -o go-template='{{(index .spec.ports 0).nodePort}}')
+echo -e "Data Api: ${minikube ip):${data_port}"
 curl "http://$(minikube ip):${data_port}/System"
 echo -e ""
