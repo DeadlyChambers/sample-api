185370d754fc2867b3ec36e9e0f975e1ff98166c
DeadlyChambers <shanechambers85@gmail.com>
2023-01-22 20:14:20 -0700

    Updating jenkinsfile

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -1,5 +1,9 @@
 libraries {
     dotnet {
+    gitUrl = "github.org"
+    companyName = "DeadlyChambers"
+    tagPrefix = "jte-"
+    newRelicAppName = "NONE"
         appName = "sample-api"
         project = "system"
         version = "1.0.0"
